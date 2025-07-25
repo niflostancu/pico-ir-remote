735c13b33544b33b95239d2e88534eb258f9435e
Stancu Florin <niflostancu@gmail.com>
2025-07-25 22:01:41 +0300

    cli: migrate to rp_toolkit

--- lib/main/commands.c
+++ lib/main/commands.c
@@ -6,7 +6,10 @@
 #include <stdint.h>
 #include <stdio.h>
 
-#include "lib/cli/cli.h"
+#include <rp_cli/cli.h>
+#include <rp_cli/rp_stdio.h>
+#include <rp_util/cc_helpers.h>
+
 #include "lib/ir_proto/capture/ircapture.h"
 
 #include "./device_mgr.h"
@@ -14,69 +17,49 @@
 
 
 // command handlers
-static pico_cli_action_ret_t main_cmd_on(struct pico_cli_state *cli,
-            const char *command, const void *args[]);
-static pico_cli_action_ret_t main_cmd_off(struct pico_cli_state *cli,
-            const char *command, const void *args[]);
-static pico_cli_action_ret_t main_cmd_apply(struct pico_cli_state *cli,
-            const char *command, const void *args[]);
+RP_CLI_STATIC_CMD(main_cmd_on);
+RP_CLI_STATIC_CMD(main_cmd_off);
+RP_CLI_STATIC_CMD(main_cmd_apply);
+RP_CLI_STATIC_CMD(main_cmd_ir_cap_start);
+RP_CLI_STATIC_CMD(main_cmd_ir_cap_stop);
+RP_CLI_STATIC_CMD(main_cmd_ir_cap_print);
 
-pico_cli_action_ret_t main_cmd_ir_cap_start(struct pico_cli_state *cli,
-            const char *command, const void *args[]);
-pico_cli_action_ret_t main_cmd_ir_cap_stop(struct pico_cli_state *cli,
-            const char *command, const void *args[]);
-pico_cli_action_ret_t main_cmd_ir_cap_print(struct pico_cli_state *cli,
-            const char *command, const void *args[]);
-
-const struct pico_cli_command_entry main_cli_commands[] = {
-    {.name = "help",
-     .action = pico_cli_default_help,
-     .help = "Show command help." },
-    {.name = "apply",
-     .action = main_cmd_apply,
-     .help = "Applies (transmits) the current settings over IR to the device." },
-    {.name = "on",
-     .action = main_cmd_on,
-     .help = "Turns the device on." },
-    {.name = "off",
-     .action = main_cmd_off,
-     .help = "Turns the device off." },
-    {.name = "ir_cap_start",
-     .action = main_cmd_ir_cap_start,
-     .help = "Starts raw IR capture." },
-    {.name = "ir_cap_stop",
-     .action = main_cmd_ir_cap_stop,
-     .help = "Stops the raw IR capture." },
-    {.name = "ir_cap_print",
-     .action = main_cmd_ir_cap_print,
-     .help = "Starts raw IR capture." },
+/** Simple CLI instance */
+struct rp_cli_default_desc main_cli;
+const struct rp_cli_cmd_entry main_cli_commands[] = {
+    RP_CLI_HELP_CMD_ENTRY(&main_cli),
+    {"apply", "Applies (transmits) the current settings over IR to the device.",
+     "", main_cmd_apply},
+    {"on", "Turns the device on.", "", main_cmd_on },
+    {"off", "Turns the device off.", "", main_cmd_off },
+    {"ir_cap_start", "Starts raw IR capture.", "", main_cmd_ir_cap_start },
+    {"ir_cap_stop", "Stops the raw IR capture.", "", main_cmd_ir_cap_stop },
+    {"ir_cap_print", "Starts raw IR capture.", "", main_cmd_ir_cap_print },
 };
 
-static struct pico_cli_state cli;
-
-/** The MCU timestamp to end IR capture at. */
-static uint64_t _ir_capture_end_us = 0;
+const struct rp_cli_cmd_registry main_reg = {
+    .entries = main_cli_commands,
+    .entries_count = RP_ARRAY_SIZE(main_cli_commands),
+};
 
 /**
  * Initializes the main CLI commands.
  */
 void main_cmd_init()
 {
-    pico_cli_init(&cli, main_cli_commands, PICO_CLI_ARRAY_SIZE(main_cli_commands));
+    /* initialize RP CLI stdio library */
+    rp_cli_def_init(&main_cli, &main_reg, &rp_cli_stdio_funcs);
     ir_capture_init();
 }
 
 /** Runs the main CLI process. */
 void main_cli_process()
 {
-    pico_cli_process(&cli);
-    if (_ir_capture_end_us && (_ir_capture_end_us < time_us_64())) {
-
-    }
+    rp_cli_def_process(&main_cli);
 }
 
-pico_cli_action_ret_t main_cmd_on(struct pico_cli_state *cli,
-            const char *command, const void *args[])
+pico_cli_action_ret_t main_cmd_on(int argc, const char *argv[],
+        void *aux_data)
 {
     int res;
     if ((res = device_exec_basic(DEVICE_CMD_ON)) < 0) {
@@ -87,8 +70,8 @@ pico_cli_action_ret_t main_cmd_on(struct pico_cli_state *cli,
     return 0;
 }
 
-pico_cli_action_ret_t main_cmd_off(struct pico_cli_state *cli,
-            const char *command, const void *args[])
+pico_cli_action_ret_t main_cmd_off(int argc, const char *argv[],
+        void *aux_data)
 {
     int res;
     if ((res = device_exec_basic(DEVICE_CMD_OFF)) < 0) {
@@ -99,31 +82,31 @@ pico_cli_action_ret_t main_cmd_off(struct pico_cli_state *cli,
     return 0;
 }
 
-pico_cli_action_ret_t main_cmd_apply(struct pico_cli_state *cli,
-            const char *command, const void *args[])
+pico_cli_action_ret_t main_cmd_apply(int argc, const char *argv[],
+        void *aux_data)
 {
     printf("Not implemented yet!\r\n");
     return 0;
 }
 
-pico_cli_action_ret_t main_cmd_ir_cap_start(struct pico_cli_state *cli,
-            const char *command, const void *args[])
+pico_cli_action_ret_t main_cmd_ir_cap_start(int argc, const char *argv[],
+        void *aux_data)
 {
     ir_capture_start();
     printf("Raw IR capture started!\r\n");
     return 0;
 }
 
-pico_cli_action_ret_t main_cmd_ir_cap_stop(struct pico_cli_state *cli,
-            const char *command, const void *args[])
+pico_cli_action_ret_t main_cmd_ir_cap_stop(int argc, const char *argv[],
+        void *aux_data)
 {
     ir_capture_stop();
     printf("Raw IR capture stopped!\r\n");
     return 0;
 }
 
-pico_cli_action_ret_t main_cmd_ir_cap_print(struct pico_cli_state *cli,
-            const char *command, const void *args[])
+pico_cli_action_ret_t main_cmd_ir_cap_print(int argc, const char *argv[],
+        void *aux_data)
 {
     uint32_t print_max = 0;
     uint32_t print_offset = 0;
